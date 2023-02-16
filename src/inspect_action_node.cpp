4d59471ca8b576c7cb74c7ee185f3b4f28b9b44b
Jeroen <j.zwanepol@hotmail.com>
2023-02-16 15:43:22 +0100

    package builds correctly with three actions

--- src/inspect_action_node.cpp
+++ src/inspect_action_node.cpp
@@ -20,8 +20,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
+#include "rclcpp_components/register_node_macro.hpp"
+
 #include "mros2_msgs/action/control_qos.hpp"
 #include "std_msgs/msg/bool.hpp"
+#include<sstream>
 #include<string.h>
 #include<ctime>
 
@@ -30,28 +33,27 @@ using namespace std::chrono_literals;
 class InspectAction : public plansys2::ActionExecutorClient
 {
 public:
+  using ControlQos = mros2_msgs::action::ControlQos;
+  using GoalHandleControlQos = rclcpp_action::ClientGoalHandle<ControlQos>;
+
   InspectAction()
   : plansys2::ActionExecutorClient("inspect_pipeline", 250ms)
   {
     not_started = false;
     pipeline_inspected = false;
     client_cb_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-    detected_subscriber = this->create_subscription<std_msgs::msg::Bool>(
-      "pipeline/inspected", 10, std::bind(&InspectAction::detected_callback, this, _1), client_cb_group_
+    auto sub_opt = rclcpp::SubscriptionOptions();
+    sub_opt.callback_group = client_cb_group_;
+    inspected_subscriber = this->create_subscription<std_msgs::msg::Bool>(
+      "pipeline/inspected", 10, std::bind(&InspectAction::inspected_callback, this, std::placeholders::_1), sub_opt
     );
-    client = this->create_cliet<mros2_msgs::action::ControlQos>("mros/objective");
-    while (!client->wait_for_service(0s)) {
-      if (!rclcpp::ok()) {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
-        return -1;
-      }
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
-    }
+    this->client = rclcpp_action::create_client<ControlQos>(this,"mros/objective");
   }
 
 private:
   void do_work()
   {
+    using namespace std::placeholders;
     send_feedback(0.0, "Started inspect action");
     std::stringstream obj_1;
     std::stringstream obj_2;
@@ -60,38 +62,86 @@ private:
     obj_1<<"obj_"<<function_1;
     obj_2<<"obj_"<<function_2;
 
-    if not_started {
-      mros2_msgs::action::ControlQos request = std::make_shared<mros2_msgs:action::ControlQos::request>();
-      request.qos_expected.objective_type = function_1;
-      obj_1<<"_"<<this->get_clock()->now();
-      request.qos_expected.objective_id = obj_1.str();
-      request.qos_expected.select_mode = "";
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sending adaptation goal "+obj_1);
-      auto goal_future = client->async_send_request(request);
-      // if (rclcpp::spin_until_future_complete(this, result) ==
-      //   rclcpp::executor::FutureReturnCode::SUCCESS)
-      // {
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Adaptation goal sent!!!");
-      // } else {
-      //   RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service for setting objective");    // CHANGE
-      // }
+    if (not_started) {
+      if (!this->client->wait_for_action_server()) {
+        RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+        rclcpp::shutdown();
+      }
+      auto goal_msg = ControlQos::Goal();
+      goal_msg.qos_expected.objective_type = function_1;
+      obj_1<<"_"<<this->get_clock()->now().seconds();
+      goal_msg.qos_expected.objective_id = obj_1.str();
+      goal_msg.qos_expected.selected_mode = "";
+
+      auto send_goal_options = rclcpp_action::Client<ControlQos>::SendGoalOptions();
+      send_goal_options.goal_response_callback =
+        std::bind(&InspectAction::goal_response_callback, this, _1);
+      send_goal_options.feedback_callback =
+        std::bind(&InspectAction::feedback_callback, this, _1, _2);
+      send_goal_options.result_callback =
+        std::bind(&InspectAction::result_callback, this, _1);
+      this->client->async_send_goal(goal_msg, send_goal_options);
     }
 
-    if pipeline_inspected {
-      goal_future.result().async_cancel_goal();
+    if (pipeline_inspected) {
+      if (!this->client->wait_for_action_server()) {
+        RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+        rclcpp::shutdown();
+      }
+
+      this->client->async_cancel_all_goals();
+      RCLCPP_INFO(this->get_logger(), "Inspect is done");
+
       finish(true, 1.0, "Inspect completed");
     }
   }
 
-  void detected_callback(const std_msgs::msg::Bool::SharedPtr msg) const{
-    pipeline_inspected = msg.data;
+  void inspected_callback(const std_msgs::msg::Bool::SharedPtr msg) {
+    pipeline_inspected = msg->data;
     RCLCPP_INFO(this->get_logger(), "Pipeline Inspected Received");
   }
 
   bool not_started;
   bool pipeline_inspected;
-  rclcpp::Client<mros2_msgs::action::ControlQos>::SharedPtr client; 
-  rclcpp::Subscription<<<std_msgs::msg::Bool>::SharedPtr detected_subscriber;
+  rclcpp_action::Client<ControlQos>::SharedPtr client; 
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr inspected_subscriber;
+  rclcpp::CallbackGroup::SharedPtr client_cb_group_;
+
+  void goal_response_callback(GoalHandleControlQos::SharedPtr future)
+  {
+    auto goal_handle = future.get();
+    if (!goal_handle) {
+      RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+    } else {
+      RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+    }
+  }
+
+  void feedback_callback(
+    GoalHandleControlQos::SharedPtr,
+    const std::shared_ptr<const ControlQos::Feedback> feedback)
+  {
+    RCLCPP_INFO(this->get_logger(), "Performing Inspect Action");
+  }
+
+  void result_callback(const GoalHandleControlQos::WrappedResult & result)
+  {
+    switch (result.code) {
+      case rclcpp_action::ResultCode::SUCCEEDED:
+        break;
+      case rclcpp_action::ResultCode::ABORTED:
+        RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+        return;
+      case rclcpp_action::ResultCode::CANCELED:
+        RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+        return;
+      default:
+        RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+        return;
+    }
+    RCLCPP_INFO(this->get_logger(), "Inspect is done");
+    rclcpp::shutdown();
+  }
 };
 
 int main(int argc, char ** argv)
